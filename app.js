0d16c7b3eb4a9eb7c8cff171cdbe9fd3434c3e63
dr4g0nB <barbaracalle10i3@gmail.com>
2021-11-30 19:43:18 -0500

    new vue app

---
+++ app.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+    event: {
+      eventDate: 'August 14th - 16th',
+      eventTitle: 'Summer Festival!',
+      eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
+    },
+    newNameText: '',
+    guestName: [],
+  },
+  methods: {
+    formSubmitted: function() {
+      //this allows access to all properties of newNameText
+      this.guestName.push(this.newNameText)
+      this.newNameText = ''
+    }
+  },
+});
